8abcab40e0e892208b8dba0d2d082c06f163d50e
Danny Jung <danny.jung@fphcare.co.nz>
2020-01-21 15:26:40 +1300

    Basic views & graphs created

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <Vote msg="Welcome to WherEat!"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Vote from './components/Vote.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Vote
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueGraph from 'vue-graph'
+ 
+Vue.use(VueGraph)
 
 Vue.config.productionTip = false
 
